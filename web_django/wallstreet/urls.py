f55236964b5adaf97dd1ce1be05cb663c8e8cbea
marco <marco@SAM900>
2015-11-08 22:23:09 +0100

    Added Django  authentication

--- web_django/wallstreet/urls.py
+++ web_django/wallstreet/urls.py
@@ -3,13 +3,27 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+
+	# /
+
     url(r'^$', views.index, name='index'),
     
-    # get_eod (symbol, start_date)
+    
+    # /login
+    
+    url(r'^login', views.login, name='login'),
+    
+
+    # /logout
+    
+    url(r'^logout', views.logout, name='logout'),
+    
+    
+    # /get_eod/
+    
     url(r'^get_eod/(?P<symbol>\D+)/(?P<start_date>\d{4}-\d{2}-\d{2})/(?P<end_date>\d{4}-\d{2}-\d{2})', 
 		views.get_eod, name='get_eod'),
     url(r'^get_eod/(?P<symbol>\D+)/(?P<start_date>\d{4}-\d{2}-\d{2})', views.get_eod, name='get_eod'),
     url(r'^get_eod/(?P<symbol>\D+)', views.get_eod, name='get_eod'),
     
-    ### \d{4}-\d{2}-\d{2} --> 2015-31-01
 ]
--- web_django/wallstreet/views.py
+++ web_django/wallstreet/views.py
@@ -1,3 +1,5 @@
+import pdb
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -8,13 +10,58 @@ from django.conf import settings
 import json
 from lib.database import DB
 
+#~ from django.contrib.auth import authenticate, login
+import django.contrib.auth as dAuth
+
 
 def index(request):
-    return render(request, 'wallstreet/index.html')
+	
+	if not request.user.is_authenticated():
+		#~ return render(request, 'wallstreet/login.html')
+		return HttpResponse('User not authenticated')
+	
+	return render(request, 'wallstreet/plot.html')
     
     
 def get_eod(request, symbol, start_date=None, end_date=None):
 
+	if not request.user.is_authenticated():
+		msg = { error: 'Not authenticated' }
+		return HttpResponse(json.dumps(msg), content_type="application/json")
+		
 	db = DB(settings.YAHOO_DATABASE)
 	rows = db.get_eod(symbol, start_date, end_date)
 	return HttpResponse(json.dumps(rows), content_type="application/json")
+
+
+def login(request):
+
+	if request.method == 'GET':
+		
+		return render(request, 'wallstreet/login.html')
+		
+	elif request.method == 'POST':
+		
+		username = request.POST.get('inputName', None)
+		password = request.POST.get('inputPassword', None)
+		
+		user = dAuth.authenticate(username=username, password=password)
+	
+		if user is not None:
+		
+			if user.is_active: 
+				
+				dAuth.login(request, user)
+				return render(request, 'wallstreet/plot.html')
+				
+			else:
+				return HttpResponse('User not active')
+		
+		else:
+			return HttpResponse('Invalid login')
+
+
+def logout(request):
+	
+	dAuth.logout(request)
+	return render(request, 'wallstreet/login.html')
